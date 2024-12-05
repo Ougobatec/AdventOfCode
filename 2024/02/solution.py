a9b92be67e75aed5eeadb1839266737c83c0b725
Hugo Battaglia <hugo.battaglia@hotmail.fr>
2024-12-06 00:37:38 +0100

    Année 2024 : Solution 02

--- 2024/02/solution.py
+++ 2024/02/solution.py
@@ -1,9 +1,17 @@
 # Solution pour le jour 02 de l'Advent of Code 2024
 
-with open("input.txt") as f: text = f.read()
+with open("input.txt") as f: lines = f.readlines()
 
-res1 = 0
+reports = [list(map(int, line.strip().split())) for line in lines]
+
+def test(levels):
+    if levels == sorted(levels) or levels == sorted(levels, reverse=True):
+        for i in range(len(levels) - 1):
+            if not (1 <= abs(levels[i] - levels[i+1]) <= 3): return False
+        return True
+
+res1 = sum(1 for report in reports if test(report))
 print("La première réponse est", res1)
 
-res2 = 0
+res2 = sum(1 for report in reports if test(report) or any(test(report[:i] + report[i + 1:]) for i in range(len(report))))
 print("La deuxième réponse est", res2)
