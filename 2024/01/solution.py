eb54a32ad8d9653e17c8597b5315eae28468aabc
Hugo Battaglia <hugo.battaglia@hotmail.fr>
2024-12-06 00:36:14 +0100

    Année 2024 : Solution 01

--- 2024/01/solution.py
+++ 2024/01/solution.py
@@ -1,9 +1,15 @@
 # Solution pour le jour 01 de l'Advent of Code 2024
 
-with open("input.txt") as f: text = f.read()
+with open("input.txt") as f: lines = f.readlines()
 
-res1 = 0
+list1 = [list(map(int, line.strip().split()))[0] for line in lines]
+list2 = [list(map(int, line.strip().split()))[1] for line in lines]
+
+list1.sort()
+list2.sort()
+
+res1 = sum(abs(list1[i] - list2[i]) for i in range(len(list1)))
 print("La première réponse est", res1)
 
-res2 = 0
+res2 = sum(i * list2.count(i) for i in list1)
 print("La deuxième réponse est", res2)
