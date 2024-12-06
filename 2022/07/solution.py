2bb8c82569653c3840bff05a031b45d3ea0a72ad
Hugo Battaglia <hugo.battaglia@hotmail.fr>
2024-12-06 15:26:04 +0100

    Nouvelle génération des dossiers

--- 2022/07/solution.py
+++ 2022/07/solution.py
@@ -1,9 +1,9 @@
 # Solution pour le jour 07 de l'Advent of Code 2022
 
-with open("input.txt") as f: text = f.read()
+# with open("input.txt") as f: text = f.read()
 
-res1 = 0
-print("La première réponse est", res1)
+# res1 = 0
+# print("La première réponse est", res1)
 
-res2 = 0
-print("La deuxième réponse est", res2)
+# res2 = 0
+# print("La deuxième réponse est", res2)
