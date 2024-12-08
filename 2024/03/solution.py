e62bddbf3bb25d4f95a85f38d0d51d86980ad72f
Hugo Battaglia <hugo.battaglia@hotmail.fr>
2024-12-08 19:27:37 +0100

    Année 2024 : Solution 03

--- 2024/03/solution.py
+++ 2024/03/solution.py
@@ -1,9 +1,20 @@
 # Solution pour le jour 03 de l'Advent of Code 2024
 
-# with open("./2024/03/input.txt") as f: lines = [line.strip() for line in f.readlines()]
+import re
 
-# res1 = 0
-# print("La première réponse est", res1)
+with open("./2024/03/input.txt") as f: text = f.read()
 
-# res2 = 0
-# print("La deuxième réponse est", res2)
+find = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", text)
+res1 = sum(int(val[0]) * int(val[1]) for val in find)
+print("La première réponse est", res1)
+
+find = re.findall(r"(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))", text)
+res2 = 0
+test = True
+for i in find:
+    if i[0] and test:
+        values = i[0][4:][:-1].split(",")
+        res2 += int(values[0]) * int(values[1])
+    if i[1]: test = True
+    if i[2]: test = False
+print("La deuxième réponse est", res2)
